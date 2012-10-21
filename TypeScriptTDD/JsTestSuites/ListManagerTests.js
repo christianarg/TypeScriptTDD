6ce4716f133ec03e9eae22a19b2f420be41fe5a6
Christian Rodriguez <christianarg@gmail.com>
2012-10-21 11:57:38 +0200

    Refactor de los Tests, eliminar duplicidad
    Simple, hermoso e importantísimo refactor
    Utilizar la funcionalidad de qUnit para evitar la duplicación
    en cada test de crear el objeto list Manager.
    Se simplifica el código de cada test caso de modificar la manera de
    inicializar el ListManager, se toca un solo lugar

--- TypeScriptTDD/JsTestSuites/ListManagerTests.js
+++ TypeScriptTDD/JsTestSuites/ListManagerTests.js
@@ -2,44 +2,42 @@
 /// <reference path="qunit/qunit-1.10.0.js" />
 /// <reference path="../JsThirdParty/jquery-1.7.1.min.js" />
 
-// TODO: No me acuerdo como se hace el init por módulo qunit
-function init() {
-    $('#qunit-fixture').append('<div id ="theId"></div>');
-}
+var listManager = null;
+
+module("ListCreation", {
+    setup: function () {
+        listManager = new RichEdition.ListManager();
+    }
+});
+
 
 test("List from null", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList(null);
     deepEqual(listHtml, '<ul><li></li></ul>');
 });
 
 test("List from empty string", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList("");
     deepEqual(listHtml, '<ul><li></li></ul>');
 });
 
 test("List from simple text node", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList("hola");
     deepEqual(listHtml, '<ul><li>hola</li></ul>');
 });
 
 test("List from paragraphs (separated by <p> tag)", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList("<p>hola</p>");
     deepEqual(listHtml, '<ul><li>hola</li></ul>');
 });
 
 
 test("List from two paragraphs (separated by <p> tag)", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList("<p>hola</p><p>que tal</p>");
     deepEqual(listHtml, '<ul><li>hola</li><li>que tal</li></ul>');
 });
 
 test("List from two paragraphs (separated by <br /> tag)", function () {
-    var listManager = new RichEdition.ListManager();
     var listHtml = listManager.createList("hola<br />que tal<br />");
     deepEqual(listHtml, '<ul><li>hola</li><li>que tal</li></ul>');
 });
